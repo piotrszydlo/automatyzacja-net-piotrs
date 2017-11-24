2e44f1d0b73420fcf3fa36bce490e5641493ba3f
piotrszydlo <33911472+piotrszydlo@users.noreply.github.com>
2017-11-24 13:08:05 +0100

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/ProgramExit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace zadanie1
+{
+    class ProgramExit
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Aby zamknąć aplikację, należy wpisać exit i nacisnąć klawisz enter");
+                string input = Console.ReadLine();
+                Console.ReadKey();
+                if (input == "exit")
+                    break;
+            }
+            Environment.Exit(-1);
+        }
+    }
+}
