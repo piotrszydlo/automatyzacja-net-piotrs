4948a08622e390a17b6af97f370abf42381383db
piotrszydlo <piotrszydlo@outlook.com>
2017-11-28 17:33:58 +0100

    Poprawne działanie szukaczki i klikaczki ze sleepem

---
+++ Szkolenie_zad1/MySelenium/Example.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Text;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Xunit;
+using System.Linq;
+
+namespace SeleniumTests
+{
+    public class Example : IDisposable
+    {
+        private IWebDriver driver;
+        private StringBuilder verificationErrors;
+        private string baseURL;
+        private bool acceptNextAlert = true;
+
+        public Example()
+        {
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+//            driver.Manage().Timeouts().ImplicitWait
+            baseURL = "https://www.google.pl/";
+            verificationErrors = new StringBuilder();
+        }
+        
+
+
+        [Fact]
+        public void TheExampleTest()
+        {
+
+           
+
+            driver.Navigate().GoToUrl(baseURL);
+            driver.FindElement(By.Id("lst-ib")).Clear();
+            driver.FindElement(By.Id("lst-ib")).SendKeys("code sprinters");
+            driver.FindElement(By.Id("lst-ib")).Submit();
+            driver.FindElement(By.LinkText("Code Sprinters -")).Click();
+
+            var element = driver.FindElement(By.LinkText("Poznaj nasze podejście"));
+
+            Assert.NotNull(element);
+
+            var elements = driver.FindElements(By.LinkText("Poznaj nasze podejście"));
+
+            Assert.Single(elements);
+
+            driver.FindElement(By.Id("cookie_action_close_header")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+            wait.Until(ExpectedConditions.InvisibilityOfElementWithText(By.LinkText("Akceptuję"), "Akceptuję"));
+
+            waitForElementPresent(By.LinkText("Poznaj nasze podejście"), 2);
+
+            driver.FindElement(By.LinkText("Poznaj nasze podejście")).Click();
+
+            //ver1
+            Assert.Contains("WIEDZA NA PIERWSZYM MIEJSCU", driver.PageSource);
+
+            //ver2
+            Assert.Single(driver.FindElements(By.TagName("h2"))
+                .Where(tag => tag.Text == "WIEDZA NA PIERWSZYM MIEJSCU"));
+
+        }
+
+        protected void waitForElementPresent(By by, int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        protected void waitForElementPresent(IWebElement by, int seconds)
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(seconds));
+            wait.Until(ExpectedConditions.ElementToBeClickable(by));
+        }
+
+        //private bool IsElementPresent(By by)
+        //{
+        //    try
+        //    {
+        //        driver.FindElement(by);
+        //        return true;
+        //    }
+        //    catch (NoSuchElementException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //private bool IsAlertPresent()
+        //{
+        //    try
+        //    {
+        //        driver.SwitchTo().Alert();
+        //        return true;
+        //    }
+        //    catch (NoAlertPresentException)
+        //    {
+        //        return false;
+        //    }
+        //}
+
+        //private string CloseAlertAndGetItsText()
+        //{
+        //    try
+        //    {
+        //        IAlert alert = driver.SwitchTo().Alert();
+        //        string alertText = alert.Text;
+        //        if (acceptNextAlert)
+        //        {
+        //            alert.Accept();
+        //        }
+        //        else
+        //        {
+        //            alert.Dismiss();
+        //        }
+        //        return alertText;
+        //    }
+        //    finally
+        //    {
+        //        acceptNextAlert = true;
+        //    }
+        //}
+
+        public void Dispose()
+        {
+            try
+            {
+                driver.Quit();
+            }
+            catch (Exception)
+            {
+                // Ignore errors if unable to close the browser
+            }
+            Assert.Equal("", verificationErrors.ToString());
+        }
+    }
+}
