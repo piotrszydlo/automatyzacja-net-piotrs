9feb8342d5b344b30e49b575854939918cb53639
piotrszydlo <piotrszydlo@outlook.com>
2017-11-28 11:24:55 +0100

    Dodanie treści klasy, dodanie pakietu xunit

---
+++ Szkolenie_zad1/Szkolenie_zad1/Matematyka.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Szkolenie_zad1
+{
+    public class Matematyka
+    {
+        public double Add(double x, double y)
+        {
+            return (x + y);
+        }
+        public double Subtract(double x, double y)
+        {
+            return (x - y);
+        }
+        public double Multiply(double x, double y)
+        {
+            return (x * y);
+        }
+        public double Devide(double x, double y)
+        {
+            return (x / y);
+        }
+    }
+}
